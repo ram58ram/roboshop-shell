240301b88e80fc85875c597510cc316f551ddcd6
sivakumarreddy <info@joindevops.com>
2023-08-23 07:48:51 +0530

    added mysql

---
+++ shipping.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+yum install maven -y
+
+useradd roboshop
+
+mkdir /app
+
+unzip /tmp/shipping.zip
+
+cd /app
+
+mvn clean package
+
+mv target/shipping-1.0.jar shipping.jar
+
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
