12e533a2564c9ec13906045f5782cca5c53b4702
ram58ram <ram58ram@gmail.com>
2023-08-27 17:13:33 -0400

    making instance type dynamic

--- create-ec2-practice.sh
+++ create-ec2-practice.sh
@@ -5,7 +5,7 @@ NAMES=("mongodb" "redis")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-032f7a4b67bbaa9e4
-DOMAIN_NAME=joindevops.online
+DOMAIN_NAME=krishnadevops.tech
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
@@ -21,4 +21,17 @@ do
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
+
+    aws route53 change-resource-record-sets --hosted-zone-id Z08144973FFFLO7KE4261 --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
 done
