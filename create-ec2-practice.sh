a1b6e99b22f72cb59390796c00e66e59713d2b7c
ram58ram <ram58ram@gmail.com>
2023-08-27 16:55:06 -0400

    making instance type dynamic

--- create-ec2-practice.sh
+++ create-ec2-practice.sh
@@ -1,17 +1,24 @@
 #! /bin/bash
 
-NAMES=("mongodb" "redis" "mysql")
+##NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+NAMES=("mongodb" "redis")
 INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0189d4870cc65fd55
+DOMAIN_NAME=joindevops.online
 
-for serverName in ${NAMES[@]}
-do
-    if  [  $serverName = "mongodb" || $serverName = "mysql" ]; then
-        INSTANCE_TYPE="t3.medium"
-        echo "${INSTANCE_TYPE}"
-    else
-        INSTANCE_TYPE="t2.micro"
-         echo "${INSTANCE_TYPE}"
-    fi
-    echo "Creating server : $serverName"
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
+
+for i in "${NAMES[@]}"
+do  
+    #if [[ $i == "mongodb" || $i == "mysql" ]]
+    #then
+        #INSTANCE_TYPE="t3.medium"
+    #else
+        #INSTANCE_TYPE="t2.micro"
+    #fi
+    INSTANCE_TYPE="t2.micro"
+    echo "creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
 done
-
