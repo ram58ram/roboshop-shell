a58664c3f171e015aaf6d4e510e78154ff1863c5
ram58ram <ram58ram@gmail.com>
2023-08-27 16:17:01 -0400

    making instance type dynamic

--- create-ec2-practice.sh
+++ create-ec2-practice.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
-NAMES=("mongodb" "redis")
+NAMES=("mongodb" "redis" "mysql")
 INSTANCE_TYPE=""
 
 for serverName in ${NAMES[@]}
 do
     if  [  $serverName = "mongodb" || $serverName = "mysql" ]; then
         INSTANCE_TYPE="t3.medium"
+        echo "${INSTANCE_TYPE}"
     else
         INSTANCE_TYPE="t2.micro"
+         echo "${INSTANCE_TYPE}"
     fi
     echo "Creating server : $serverName"
 done
